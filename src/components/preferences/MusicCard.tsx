86520f07a32497623a14f454394d5662badb9a76
HS <hasang2691@naver.com>
2024-07-12 22:33:58 +0900

    Fix: Apply framer-motion and fit the size of images

--- src/components/preferences/MusicCard.tsx
+++ src/components/preferences/MusicCard.tsx
@@ -12,16 +12,18 @@ const MusicCard: React.FC<Props> = ({
   onLoad,
 }) => {
   return (
-    <div className="p-8">
-      <img
-        src={albumCover}
-        alt="Album Cover"
-        className="w-48 h-24 rounded-t-lg object-contain"
-        onLoad={onLoad}
-      />
+    <div className="p-4 w-36">
+      <div className="aspect-square mb-2 overflow-hidden">
+        <img
+          src={albumCover}
+          alt="Album Cover"
+          className="w-full h-full object-cover rounded-lg"
+          onLoad={onLoad}
+        />
+      </div>
       <div className="text-left">
-        <h3 className="text-xs font-semibold whitespace-nowrap">{songTitle}</h3>
-        <p className="text-xs text-gray-500 whitespace-nowrap">{artist}</p>
+        <h3 className="text-xs font-semibold truncate">{songTitle}</h3>
+        <p className="text-xs text-gray-500 truncate">{artist}</p>
       </div>
     </div>
   );

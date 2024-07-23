50d8c464c8fa21e3ba93a85babcf97aba95c40c2
HS <hasang2691@naver.com>
2024-07-23 15:49:11 +0900

    Feat: Create ProjectCard, ProjectsContent, ProjectArray, and Change background color

---
+++ src/constants/ProjectArray.ts
@@ -0,0 +1,87 @@
+const WebFrontEndArray = [
+  {
+    title: 'MOCU',
+    imageUrl: '/image/projects/mocu.png',
+    techStack: ['React', 'Javascript', 'TypeScript'],
+    duration: '2023.12 ~ 2024.02',
+    description: '모두의 쿠폰, MOCU (웹 프론트엔드로 참여)',
+    link: 'https://github.com/MOCU-Frontend/frontend',
+  },
+  {
+    title: 'PROlog(진행 중)',
+    imageUrl: '/image/projects/prolog_capture.png',
+    techStack: ['React', 'Javascript', 'TypeScript', 'tailwind CSS'],
+    duration: '2024.02 ~',
+    description: 'CRUD 게시판 구현 프로젝트(웹 프론트엔드로 참여)',
+    link: 'https://github.com/crohasang/prolog',
+  },
+  {
+    title: 'Covigator(진행 중)',
+    imageUrl: '/image/projects/covigator_capture.png',
+    techStack: ['React', 'Javascript', 'TypeScript', 'tailwind CSS'],
+    duration: '2024.07 ~',
+    description:
+      '공공데이터를 기반으로 한 AI 맞춤형 코스 추천 서비스(웹 프론트엔드로 참여)',
+    link: 'https://github.com/Covigator/Covigator-FE',
+  },
+];
+
+const NomadCoderArray = [
+  {
+    title: 'momentum_js',
+    imageUrl: '/image/projects/momentum_js.png',
+    techStack: ['JavaScript'],
+    duration: '2023.08',
+    description:
+      '구글 크롬 확장 프로그램 모멘텀(Momentum)을 Javascript로 구현한 프로젝트',
+    link: 'https://github.com/crohasang/momentum_js',
+  },
+  {
+    title: 'React-Practice',
+    imageUrl: '/image/projects/react-practice.png',
+    techStack: ['React', 'JavaScript'],
+    duration: '2023.08',
+    description:
+      '별점이 8.5점 이상인 영화들을 정렬해서 보여주는 사이트를 React로 구현한 프로젝트',
+    link: 'https://github.com/crohasang/React-Practice',
+  },
+  {
+    title: 'ts_hash_generator',
+    imageUrl: '/image/projects/ts_hash_generator.png',
+    techStack: ['React', 'JavaScript', 'TypeScript'],
+    duration: '2023.12',
+    description:
+      '간단한 해시 값 생성기를 만들며 Typescript 문법을 익히는 프로젝트',
+    link: 'https://github.com/crohasang/ts_hash_generator',
+  },
+  {
+    title: 'twitter-clone-firebase',
+    imageUrl: '/image/projects/twitter-clone-firebase.png',
+    techStack: ['Firebase', 'React', 'JavaScript', 'TypeScript'],
+    duration: '2024.02',
+    description: '간단한 트위터 클론코딩으로 Firebase 사용법을 익히는 프로젝트',
+    link: 'https://github.com/crohasang/twitter-clone-firebase',
+  },
+];
+
+const JetpackComposeArray = [
+  {
+    title: 'SweatNote',
+    imageUrl: '/image/projects/sweat_note.png',
+    techStack: ['Kotlin(Jetpack Compose)'],
+    duration: '2024.05 ~ 2024.06',
+    description:
+      '땀으로 쓰는 일기, SweatNote (운동 감정 기록 어플리케이션 제작 프로젝트)',
+    link: 'https://github.com/crohasang/SweatNote',
+  },
+  {
+    title: 'EmergencyGuide',
+    imageUrl: '/image/projects/emergency_guide.png',
+    techStack: ['Kotlin(Jetpack Compose)'],
+    duration: '2024.05 ~ 2024.06',
+    description: '응급 상황 정보 제공 어플리케이션 제작 프로젝트',
+    link: 'https://github.com/crohasang/EmergencyGuide',
+  },
+];
+
+export { WebFrontEndArray, NomadCoderArray, JetpackComposeArray };
